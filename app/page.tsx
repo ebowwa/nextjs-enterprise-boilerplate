50bc7b07428522602b3e2cd765c339fb9e428aed
ebowwa <81942069+ebowwa@users.noreply.github.com>
2024-01-21 04:09:45 +0000

    purplewood

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "components/Button/Button"
 import { LP_GRID_ITEMS } from "lp-items"
 
 export const metadata: Metadata = {
-  title: "Diego Construction",
+  title: "purplewood landscaping",
   twitter: {
     card: "summary_large_image",
   },
@@ -26,12 +26,10 @@ export default function Web() {
         <div className="mx-auto grid max-w-screen-xl px-4 py-8 text-center lg:py-16">
           <div className="mx-auto place-self-center">
             <h1 className="mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight dark:text-white md:text-5xl xl:text-6xl">
-              Next.js Enterprise Boilerplate
+              purplewood landscaping
             </h1>
             <p className="mb-6 max-w-2xl font-light text-gray-500 dark:text-gray-400 md:text-lg lg:mb-8 lg:text-xl">
-              Jumpstart your enterprise project with our feature-packed, high-performance Next.js boilerplate!
-              Experience rapid UI development, AI-powered code reviews, and an extensive suite of tools for a smooth and
-              enjoyable development process.
+              Concrete, stamped concrete, Pavers, Drainage, Fencing, irrigation, Sod, artificial turf, retaining wall, tree removal and pruining, mulch, planting everything around landscaping 
             </p>
             <Button href="https://github.com/Blazity/next-enterprise" className="mr-3">
               Get started
